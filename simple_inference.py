4ffcfb13d2e7a1497194eba54571bd3d8ccca835
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-01-31 16:13:18 +0000

    * get rid of polymer

--- simple_inference.py
+++ simple_inference.py
@@ -5,3 +5,37 @@ from types import SimpleNamespace
 import wandb
 
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+from utils import parse_args
+
+WANDB_PROJECT = "mixtral"
+WANDB_ENTITY = "capecape"
+
+config = SimpleNamespace(
+    model_id = "mistralai/Mixtral-8x7B-v0.1",
+    text = "Tell me a story about planes and cats, make it Tolkien-esque: ",
+    use_flash_attention_2=False,
+    load_in_4bit=False,
+    load_in_8bit=False,
+    use_cache=True,
+    max_new_tokens=100,
+)
+
+
+def main(config):
+    tokenizer = AutoTokenizer.from_pretrained(config.model_id)
+
+    model = AutoModelForCausalLM.from_pretrained(
+        config.model_id, 
+        use_flash_attention_2=config.use_flash_attention_2,
+        load_in_4bit=config.load_in_4bit and not config.load_in_8bit,
+        load_in_8bit=config.load_in_8bit and not config.load_in_4bit,
+        use_cache=config.use_cache,
+        device_map="auto",
+    )
+
+    
+    inputs = tokenizer(config.text, return_tensors="pt").to(0)
+    t = time.perf_counter()
+    outputs = model.generate(**inputs, max_new_tokens=config.max_new_tokens)
