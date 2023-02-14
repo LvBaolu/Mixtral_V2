d22234858e8f2bdc7594e0f9b56f9a0047b4614f
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-02-14 21:01:46 +0000

    Bump jinja2 from 2.11.2 to 2.11.3

--- simple_train.py
+++ simple_train.py
@@ -4,4 +4,5 @@ import wandb
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
-from trl import SFT
+from trl import SFTTrainer
+from datasets import load_d
