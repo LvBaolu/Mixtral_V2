cf5a4f7dddcf93db2bb8db192a0ebd93afc308f3
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-03-16 12:06:03 +0000

    something new

--- simple_train.py
+++ simple_train.py
@@ -19,4 +19,5 @@ config = SimpleNamespace(
     dataset_id = "c-s-ale/alpaca-gpt4-data",
     split = "train",
     model_id = "mistralai/Mixtral-8x7B-v0.1",
-    batch_size = 1, # what my GPU can handle, depends on how many layers are 
+    batch_size = 1, # what my GPU can handle, depends on how many layers are we training
+    effective_batch_si
