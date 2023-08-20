d91ff1fa3e8be2ebf48ca74bd509d381f1a808ae
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-08-20 20:54:22 +0000

    Update .appversions

--- utils.py
+++ utils.py
@@ -18,4 +18,4 @@ def parse_args(config):
     print("Running with the following config")
     parser = argparse.ArgumentParser(description='Run training baseline')
     for k,v in config.__dict__.items():
-        parser.add_argument('--'+k, type=t
+        parser.add_argument('--'+k, type=type(v) if type(v) is 
