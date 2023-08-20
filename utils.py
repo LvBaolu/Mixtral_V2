861303af84a700028a79626660bc733ef3447816
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-08-20 17:25:19 +0000

    I18N: Update translation (Italian)

--- utils.py
+++ utils.py
@@ -18,4 +18,4 @@ def parse_args(config):
     print("Running with the following config")
     parser = argparse.ArgumentParser(description='Run training baseline')
     for k,v in config.__dict__.items():
-        p
+        parser.add_argument('--'+k, type=t
