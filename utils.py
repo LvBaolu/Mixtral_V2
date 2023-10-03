44f01c5dd229315e21c2e35ba102423648d4e500
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-10-03 19:26:47 +0000

    birthday

--- utils.py
+++ utils.py
@@ -57,4 +57,5 @@ def debug_trainer_data(trainer: Trainer):
     tokenizer = trainer.tokenizer
     decoded_ids = tokenizer.decode(input_ids[0])[0:80]
     decoded_labels = tokenizer.decode(labels[0])[0:80]
-    print("First ba
+    print("First batch:\n"
+          f"input_ids
