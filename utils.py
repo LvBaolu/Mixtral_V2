83f38928defd1ebcc400b9b0e8cbd29b17e635b4
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-10-15 20:43:01 +0000

    Update changes.

--- utils.py
+++ utils.py
@@ -70,4 +70,4 @@ def _prompt_no_input(row):
 def _prompt_input(row):
     return ("Below is an instruction that describes a task, paired with an input that provides further context. "
             "Write a response that appropriately completes the request.\n\n"
-            "### Instructio
+            "### Instruction:\n{instruction}\n\n### Input
