881c107995fac808049fd526f817341acdf8c90c
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-10-20 12:50:12 +0000

    Refactoring step 3 cleaning up

--- utils.py
+++ utils.py
@@ -70,4 +70,4 @@ def _prompt_no_input(row):
 def _prompt_input(row):
     return ("Below is an instruction that describes a task, paired with an input that provides further context. "
             "Write a response that appropriately completes the request.\n\n"
-            "### Instruction:\n{instruction}\n\n### Input
+            "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n").format_m
