72465183011fbfaa08be644fbbbc971b7929470b
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-10-20 17:08:59 +0000

    updated layouts

--- utils.py
+++ utils.py
@@ -70,4 +70,6 @@ def _prompt_no_input(row):
 def _prompt_input(row):
     return ("Below is an instruction that describes a task, paired with an input that provides further context. "
             "Write a response that appropriately completes the request.\n\n"
-            "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n").format_m
+            "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n").format_map(row)
+
+def create_alpaca_prompt(r
