d97a1b8b6e397e4b034d2af44d59aa30277dac78
LvBaolu <100171849+LvBaolu@users.noreply.github.com>
2023-10-22 22:02:31 +0000

    Remove unused files

--- utils.py
+++ utils.py
@@ -76,4 +76,4 @@ def create_alpaca_prompt(row):
     return _prompt_no_input(row) if row["input"] == "" else _prompt_input(row)
 
 def create_alpaca_prompt_with_response(row):
-    instruct = _prompt_no_input(row) if row["input"] 
+    instruct = _prompt_no_input(row) if row["input"] == "" else _prompt_input(r
